70f4689a9e5355084d9d593a6555f0eec93460a3
KidoYuta <sutou000@gmail.com>
2023-11-23 23:49:09 +0900

    first-commit

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import './style/master.sass'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div></div>
+`
---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { resolve } from "path"
+import { defineConfig } from "vite"
+import vitePluginPug from "./plugins/vite-plugin-pug"
+
+export default defineConfig({
+  root: "src",
+  build: {
+    outDir: resolve(__dirname, "dist"),
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src", "index.pug"),
+      },
+    },
+  },
+  css: {
+    devSourcemap: true,
+  },
+  plugins: [vitePluginPug()],
+})
